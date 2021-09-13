436efdff8acc53e32c62f12fc1ec2606f9048d26
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2021-09-13 22:03:57 +0530

    Create variables.tf

---
+++ Challenge-1/variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+    type = string
+    default = "us-central"
+}
+
+variable "project" {
+    type = string
+}
+
+variable "user" {
+    type = string
+}
+
+variable "privatekeypath" {
+    type = string
+    default = "~/.ssh/id_rsa"
+}
+
+variable "publickeypath" {
+    type = string
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "firewall_name" {
+    type = string
+}
+
+variable "network" {
+    type = string
+}
+
+
+variable "instance_name" {
+    type = string
+}
+
+
+variable "machine_type" {
+    type = string
+}
+
+variable "zone" {
+    type = string
+}
+
+
+variable "os_image" {
+    type = string
+}
+
+
+variable "protocol" {
+    type = string
+}
+
+variable "serviceaccountjson"{
+ type = string 
+}
